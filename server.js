4824df66f3fb1c171563df7cf54944acb1af92ab
AStout75 <34389526+AStout75@users.noreply.github.com>
2020-08-26 14:11:17 -0400

    'auto'

--- server.js
+++ server.js
@@ -20,3 +20,178 @@ io.on('connection', (socket) => {
   });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+var rooms = {};
+var IdToRoom = {};
+var wordBank = 
+    {
+        "easy" : 
+            [
+                "pen",
+                "shirt",
+                "cup",
+            ],
+        "medium" :
+            [
+                "Eiffel Tower",
+                "whiteboard",
+                "Discord",
+            ],
+        "hard" :
+            [
+                "Bleu Cheese",
+                "Jamie Foxx",
+                "Metallica",
+            ],
+    };
+
+io.on('connect', socket => {
+
+    /*
+    Immediately upon connection, tell the client its user ID 
+    for improving client side operations */
+
+    io.to(socket.id).emit('client-id-notification', socket.id);
+
+    /* Upon joining a room, send a new room-data emit to all room
+    members */
+    socket.on('join-room', (key, name) => {
+        if (Object.keys(rooms).includes(key)) {
+            if (rooms[key]["playerCount"] == 8) {
+                socket.emit('join-room-fail');
+            }
+            else {
+                rooms[key]["playerCount"] += 1;
+                var socketId = socket.id;
+                addToFirstAvailableTeam(key, name, socketId);
+                socket.join(key, function() {
+                    IdToRoom[socketId] = key;
+                    socket.emit('join-room-success', key, name);
+                    io.in(key).emit('room-data', rooms[key]);
+                });
+        
+            }
+        }
+        else {
+            socket.emit('join-room-fail');
+        }
+        
+        
+    });
+
+    socket.on('create-room', name => {
+        var key = createNewRoomKey();
+        createNewRoom(key, name, socket);
+        var socketId = socket.id;
+        socket.join(key, function() {
+            IdToRoom[socketId] = key;
+            socket.emit('create-room-success', key);
+            io.in(key).emit('room-data', rooms[key]);
+        });
+        
+    });
+
+    
+    socket.on('join-team', (key, name, teamNumber) => {
+        removePlayerFromAnyTeam(key, socket.id);
+        if (!addToDesiredTeam(key, name, socket.id, teamNumber)) {
+            //This shouldn't happen unless someone is calling their own code
+            addToFirstAvailableTeam(key, name, socket.id);
+        }
+        io.in(key).emit('room-data', rooms[key]);
+    }); 
+
+    socket.on('leave-room', key => {
+        var socketId = socket.id;
+        IdToRoom[socketId] = null;
+        socket.leave(key);
+        rooms[key]["playerCount"] -= 1;
+        removePlayerFromAnyTeam(key, socket.id);
+        io.in(key).emit('room-data', rooms[key]);
+        socket.disconnect;
+    });
+
+    socket.on('disconnect', function() {
+        //Find out which room they were in
+        var socketId = socket.id;
+        var key = IdToRoom[socketId];
+        if (key != null) {
+            rooms[key]["playerCount"] -= 1;
+            io.in(key).emit('room-data', rooms[key]);
+        }
+        
+    });
+});
+
+function addToFirstAvailableTeam(key, name, playerId) {
+    if (rooms[key]["team1"].length < 4) {
+        var newMember = {};
+        newMember[playerId] = name;
+        rooms[key]["team1"].push(newMember);
+    }
+    else {
+        var newMember = {};
+        newMember[playerId] = name;
+        rooms[key]["team2"].push(newMember);
+    }
+}
+
+function addToDesiredTeam(key, name, playerId, number) {
+    if (rooms[key]["team".concat(number)].length < 4) {
+        var newMember = {};
+        newMember[playerId] = name;
+        rooms[key]["team".concat(number)].push(newMember);
+        return true;
+    }
+    return false;
+}
+
+
+
+function removePlayerFromAnyTeam(key, playerId) {
+    var team1 = rooms[key]["team1"];
+    var team2 = rooms[key]["team2"];
+    for (var i = 0; i < team1.length; i++) {
+        if (Object.keys(team1[i])[0] == playerId) {
+            team1.splice(i, i + 1);
+            return;
+        }
+    }
+
+    //not in team 1...
+    for (var i = 0; i < team2.length; i++) {
+        if (Object.keys(team2[i])[0] == playerId) {
+            team2.splice(i, i + 1);
+            return;
+        }
+    }
+}
+
+
+
+//Generate a sequence of 4 random 0-9 numbers that doesn't
+// already exist in rooms
+
+function createNewRoomKey() {
+    var key = "-1";
+    while (key == -1 || key in Object.keys(rooms)) {
+        var num1 = Math.floor((Math.random() * 10)).toString();
+        var num2 = Math.floor((Math.random() * 10)).toString();
+        var num3 = Math.floor((Math.random() * 10)).toString();
+        var num4 = Math.floor((Math.random() * 10)).toString();
+        key = num1 + num2 + num3 + num4;
+    }
+    return key;
+    
+}
+
+function createNewRoom(key, hostName, socket) {
+    var hostId = socket.id;
+    rooms[key] = {};
+    rooms[key]["playerCount"] = 1;
+    rooms[key]["team1"] = [];
+    var newMember = {};
+    newMember[hostId] = hostName;
+    rooms[key]["team1"].push(newMember);
+    rooms[key]["team2"] = [];
+}
